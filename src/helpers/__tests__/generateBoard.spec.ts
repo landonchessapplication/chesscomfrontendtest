d030773b691aa4c4d2465f1ee1884a8b36632122
Landon Chess Application <landonmarshall1997+chess@gmail.com>
2024-07-03 15:37:16 -0700

    Refactor generateBoard, add new theme

--- src/helpers/__tests__/generateBoard.spec.ts
+++ src/helpers/__tests__/generateBoard.spec.ts
@@ -1,5 +1,5 @@
+import type { File, Rank } from '@/types'
 import { describe, expect, it } from 'vitest'
-
 import { generateBoard } from '../generateBoard'
 
 describe('generateBoard', () => {
@@ -11,5 +11,36 @@ describe('generateBoard', () => {
   it('Returns a board without any highlighted squares', () => {
     const board = generateBoard()
     expect(board.every((square) => square.highlighted)).not.toBeTruthy()
+  })
+  it('Returns a dark square at A1', () => {
+    // arrange
+    const file: File[number] = 'a'
+    const rank: Rank[number] = '1'
+    const board = generateBoard()
+
+    // act
+    const result = board.find(
+      (square) => square.file === file && square.rank === rank
+    )?.isDarkSquare
+
+    // assert
+    expect(result).toBeTruthy()
+  })
+  it('Returns a light square for E4', () => {
+    // arrange
+    const file: File[number] = 'e'
+    const rank: Rank[number] = '4'
+    const board = generateBoard()
+
+    // act
+    const result = board.find(
+      (square) => square.file === file && square.rank === rank
+    )?.isDarkSquare
+
+    // assert
+    expect(result).not.toBeTruthy()
+
+    // assert
+    expect(result).not.toBeTruthy()
   })
 })
--- src/helpers/generateBoard.ts
+++ src/helpers/generateBoard.ts
@@ -1,5 +1,16 @@
 import { BOARD_FILES, BOARD_RANKS } from '@/static'
-import { isDarkSquare } from './isDarkSquare'
+
+/**
+ * @description This function checks whether the square at a given file and rank is a dark square.
+ * On even files (b,d,f,h), even ranks (2,4,6,8) are considered dark
+ * On odd files (a,c,e,g) odd ranks (1,3,5,7) are considered dark
+ * @param fileIndex {number} The fileIndex to check
+ * @param rankIndex {number} The rankIndex to check
+ * @returns {boolean} Whether the square is a dark square
+ */
+const isDarkSquare = (fileIndex: number, rankIndex: number) => {
+  return (rankIndex + fileIndex) % 2 === 1
+}
 
 /**
  * @description This function is used to generate the chess board
@@ -11,7 +22,7 @@ export const generateBoard = () => {
       file,
       rank,
       index: rankIndex * BOARD_RANKS.length + fileIndex,
-      isDarkSquare: isDarkSquare(file, rank),
+      isDarkSquare: isDarkSquare(fileIndex, rankIndex),
       highlighted: false
     }))
   )
