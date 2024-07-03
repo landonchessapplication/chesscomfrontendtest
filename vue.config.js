f23f16770c76b07dcf4d84284166f2ac70eca64d
Landon Chess Application <landonmarshall1997+chess@gmail.com>
2024-07-03 09:52:28 -0700

    Config for github pages

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
-module.exports = {
+import { defineConfig } from 'vite'
+module.exports = defineConfig({
+  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/chesscomfrontendtest/' : '/'
-}
+})
